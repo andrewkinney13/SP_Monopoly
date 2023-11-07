f2aa3785ea3975b294ff993d4831eae5a9ffe745
Andrew Kinney <andrewkinney13@gmail.com>
2023-11-07 10:57:37 -0500

    Adding dice functionality

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,31 +4,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpaceButtonManager : MonoBehaviour
+public class Board : MonoBehaviour
 {
+    // Buttons
     public List<Button> spaceButtons = new List<Button>();
+    //public Button diceButton;
+
+    // Game objects
+    
 
     // Initialize all the space buttons
     void Start()
     {
-        // Assign the OnClick function
+        // Space Button
         int index = 0;
         foreach (Button button in spaceButtons) 
         {
             int buttonIndex = index;   // locally scoped variable, for function assignment
-            button.onClick.AddListener(() => OnClick(buttonIndex));
+            button.onClick.AddListener(() => OnSpaceClick(buttonIndex));
             index++;
         }
+
+        // Dice Button
+        //diceButton.onClick.AddListener(OnDiceClick);
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
-    void OnClick(int spaceIndex)
+   
+    void OnSpaceClick(int spaceIndex)
     {
         Debug.Log("Hey! User clicked the..." + spaceIndex + " space!");
     }
+
+    // 
+    /*
+    void OnDiceClick()
+    {
+
+    }    */
 }
