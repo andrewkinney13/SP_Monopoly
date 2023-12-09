4713c8a4651a59bd5fee35399c939c125531cfe6
Andrew Kinney <andrewkinney13@gmail.com>
2023-12-09 13:52:18 -0500

    removed player panel class and reintegrated functionality into gamecontroller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,16 +10,47 @@ public class GameController : MonoBehaviour
 {
     // Data Members
     private Board m_board;
-    private int m_turnNum;    // whose turn it is
+    private int m_turnNum;    
+    private bool m_turnCompleted = false;       
+    private bool m_panelUpdateNeeded = false; 
 
+    // Buttons the user can click to get info about stuff
     public List<Button> m_spaceButtons;
     public List<Button> m_playerButtons;
-    public List<Sprite> m_icons;
 
+    // Controller classes for buttons above
     public DetailsPopupController m_spaceDetailsController;
     public DetailsPopupController m_playerDetailsController;
+
+    // Controller class for player icon movememnt
     public PlayerTrackController m_playerTrackController;
-    public PlayerPanelController m_playerPanelController;
+
+    // Folder of icons a player can have as their game token
+    public List<Sprite> m_icons;
+    
+    // Player panel components
+    public Image m_panelIcon;
+    public TMP_Text m_panelTitle;
+    public TMP_Text m_panelCash;
+    public List<GameObject> m_actionWindows;
+    public RectTransform m_propertiesContent;
+
+    // Possible actions a player may have to take during their turn
+    public enum Actions
+    {
+        EndTurn = 0,
+        DetermineOrder = 1,
+        RollDice = 2,
+        UnownedProperty = 3,
+        OwnedColorProperty = 4,
+        OwnedUtility = 5,
+        OwnedRailroad = 6,
+        ChanceOrCommunityChest = 7,
+        VisitingJail = 8,
+        FreeParking = 9,
+        GoToJail = 10,
+        Tax = 11
+    }
 
     // Runs when the script is initialized, using this as a constructor
     void Start()
@@ -36,28 +67,36 @@ public class GameController : MonoBehaviour
 
         // Set the turn for first person in list (for now)
         m_turnNum = 0;
+
+        // Need to set the panel initially 
+        m_panelUpdateNeeded = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Obtain current player data
-        string name = m_board.Players[m_turnNum].Name;
-        float cash = m_board.Players[m_turnNum].Cash;
-        PlayerPanelController.Actions action = PlayerPanelController.Actions.EndTurn;
-
         // If attributes of the player change, redraw the player panel
-        if (m_playerPanelController.NeedsUpdate(name, cash, action))
+        if (m_panelUpdateNeeded)
         {
+            // Obtain current player data
+            string name = m_board.Players[m_turnNum].Name;
+            float cash = m_board.Players[m_turnNum].Cash;
+
+            // Determine action needed this turn
+            Actions action = DetermineAction();
+
             // Obtain arguements to pass to panel creator
             Sprite icon = GetIconSprite(m_board.Players[m_turnNum].Icon);
 
             // Create the player panel so player can do their turn
-            m_playerPanelController.CreatePlayerPanel(icon, name, cash, action);
+            CreatePlayerPanel(icon, name, cash, action);
+
+            // Set bool to false now that update has been made
+            m_panelUpdateNeeded = false;
         }
 
         // Check to see if the turn num
-        if (m_playerPanelController.TurnEnded)
+        if (m_turnCompleted)
         {
             // Update whose turn is is
             if (m_turnNum < m_board.Players.Count - 1)
@@ -68,7 +107,8 @@ public class GameController : MonoBehaviour
             {
                 m_turnNum = 0;
             }
-            m_playerPanelController.TurnEnded = false;
+            m_turnCompleted = false;
+            m_panelUpdateNeeded = true;
         }
 
         // Erase the space details window if user clicks
@@ -77,6 +117,18 @@ public class GameController : MonoBehaviour
             m_spaceDetailsController.CloseDetailsWindow();
             m_playerDetailsController.CloseDetailsWindow();
         }
+    }
+
+    // Creates the player panel for the player to do actions during their turn
+    public void CreatePlayerPanel(Sprite icon, string name, float cash, Actions action)
+    {
+        // Assign basic attributes
+        m_panelIcon.sprite = icon;
+        m_panelTitle.text = name + "'s Turn";
+        m_panelCash.text = "Cash: $" + cash;
+
+        // Assign action window
+        AssignActionWindow(action);
     }
 
     // When user clicks a space
@@ -90,7 +142,16 @@ public class GameController : MonoBehaviour
         m_spaceDetailsController.CreateDetailsWindow(spaceName, spaceDescription);
     }
 
-   
+    // When user clicks a player
+    void OnPlayerClick(int playerNum)
+    {
+        // Get the space info
+        string spaceDescription = "Hey this is a player!";
+
+        // Display it in the space details window, where the user clicked
+        m_playerDetailsController.CreateDetailsWindow(playerNum.ToString(), spaceDescription);
+    }
+
     // Initializes the player lanes and icons 
     public void InitializePlayerIcons()
     {
@@ -121,14 +182,62 @@ public class GameController : MonoBehaviour
         return null;
     }
 
-    // When user clicks a player
-    void OnPlayerClick(int playerNum)
+    // Returns what action needs to be taken by the current player
+    public Actions DetermineAction()
     {
-        // Get the space info
-        string spaceDescription = "Hey this is a player!";
+        // Check if the turn order has been determined
+        return Actions.EndTurn;
+    }
 
-        // Display it in the space details window, where the user clicked
-        m_playerDetailsController.CreateDetailsWindow(playerNum.ToString(), spaceDescription);
+    // Assigns the game action window according to what action the user needs to complete
+    public void AssignActionWindow(Actions action)
+    {
+        // Deactivate any other action windows
+        foreach (GameObject actionWindow in m_actionWindows)
+        {
+            actionWindow.SetActive(false);
+        }
+
+        // Get the window
+        GameObject currentActionWindow = m_actionWindows[(int)action];
+
+        // Set the window as active
+        currentActionWindow.gameObject.SetActive(true);
+
+        // Assign attributes of window depending on action
+        switch (action)
+        {
+            case Actions.DetermineOrder:
+            
+                // Obtain the dice buttons
+                Button die1 = GameObject.Find("Die 1").GetComponent<Button>();
+                Button die2 = GameObject.Find("Die 2").GetComponent<Button>();
+
+                // Assign determine order function
+                die1.onClick.AddListener(() => DetermineOrder(die1, die2));
+                die2.onClick.AddListener(() => DetermineOrder(die1, die2));
+
+                break;
+            
+            case Actions.EndTurn:
+            
+                // Assign end button function
+                Button endTurn = GameObject.Find("End Turn Button").GetComponent<Button>();
+                endTurn.onClick.AddListener(EndTurn);
+                break;
+        }
+    }
+
+    void DetermineOrder(Button die1, Button die2)
+    {
+        int diceRoll = Random.Range(2, 12);
+    }
+
+    void EndTurn()
+    {
+        m_turnCompleted = true;
     }
 
 }
+
+
--- Assets/Scripts/PlayerPanelController.cs
+++ Assets/Scripts/PlayerPanelController.cs
@@ -16,23 +16,25 @@ public class PlayerPanelController : MonoBehaviour
 
     public enum Actions
     {
-        DiceRoll = 0,
-        UnownedProperty = 1,
-        OwnedColorProperty = 2,
-        OwnedUtility = 3,
-        OwnedRailroad = 4,
-        ChanceOrCommunityChest = 5,
-        VisitingJail = 6,
-        FreeParking = 7,
-        GoToJail = 8,
-        Tax = 9,
-        EndTurn = 10
+        EndTurn = 0,
+        DetermineOrder = 1,
+        RollDice = 2,
+        UnownedProperty = 3,
+        OwnedColorProperty = 4,
+        OwnedUtility = 5,
+        OwnedRailroad = 6,
+        ChanceOrCommunityChest = 7,
+        VisitingJail = 8,
+        FreeParking = 9,
+        GoToJail = 10,
+        Tax = 11
     }
 
     private bool m_turnCompleted = false;
     private string m_currentName;
     private float m_currentCash;
     private Actions m_currentAction;
+    private int m_diceResult;
 
     // Creates the player panel for the player to do actions during their turn
     public void CreatePlayerPanel(Sprite icon, string name, float cash, Actions action)
@@ -51,33 +53,59 @@ public class PlayerPanelController : MonoBehaviour
         AssignActionWindow(action);
     }
 
+    // Assigns the game action window according to what action the user needs to complete
     public void AssignActionWindow(Actions action)
     {
+        // Deactivate any other action windows
+        foreach (GameObject actionWindow in m_actionWindows)
+        {
+            actionWindow.SetActive(false);  
+        }
+
+        // Get the window
+        GameObject currentActionWindow = m_actionWindows[(int)action];
+
+        // Set the window as active
+        currentActionWindow.gameObject.SetActive(true);
+
+        Debug.Log((int)action);
+        
+        // Assign attributes of window depending on action
         switch (action)
         {
-            case Actions.EndTurn:
+            case Actions.DetermineOrder:
+            case Actions.RollDice:
             {
-                // Assign the window from list 
-                GameObject currentActionWindow = m_actionWindows[0];    // index will eventually match enum
-                                                                        // ^ this line will be outside of switch  statement
+                    // Obtain the dice buttons
+                    Button die1 = GameObject.Find("Die 1").GetComponent<Button>();
+                    Button die2 = GameObject.Find("Die 2").GetComponent<Button>();
 
-                // Assign title
-                TMP_Text title = GameObject.Find("End Turn Title").GetComponent<TMP_Text>();
-                title.text = "Turn over...";
+                    // Assign determine order function
+                    die1.onClick.AddListener(() => DiceRoll(die1, die2));
+                    die2.onClick.AddListener(() => DiceRoll(die1, die2));
 
-                // Assign end button function
-                Button endTurn = GameObject.Find("End Turn Button").GetComponent<Button>();
-                endTurn.onClick.AddListener(EndTurn);
-                currentActionWindow.gameObject.SetActive(true);
+                break;
+            }
+            case Actions.EndTurn:
+            { 
+                    // Assign end button function
+                    Button endTurn = GameObject.Find("End Turn Button").GetComponent<Button>();
+                    endTurn.onClick.AddListener(EndTurn);
                 break;
             }
         }
     }
 
+    // Returns result of a dice roll 
+    // Source of icons: https://game-icons.net/1x1/delapouite/dice-six-faces-three.html 
+    public void DiceRoll(Button die1, Button die2)
+    {
+        DiceResult = Random.Range(2, 12);
+    }
+
     // End turn
     public void EndTurn()
     {
-
         TurnEnded = true;
     }
 
@@ -87,6 +115,13 @@ public class PlayerPanelController : MonoBehaviour
         get { return m_turnCompleted; }
         set { m_turnCompleted = value; }
     }
+    public int DiceResult
+    {
+        get { return m_diceResult;  }
+        set { m_diceResult = value;  }
+    }
+
+    // Checks the new and old parameters of the panel, if any changes, update is needed
     public bool NeedsUpdate(string newName, float newCash, Actions newAction)
     {
         if (m_currentName != newName || m_currentCash != newCash || m_currentAction != newAction)
